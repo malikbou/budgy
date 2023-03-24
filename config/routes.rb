94b605423d5ddc7b1a680de67341321a68c3489f
Malik Bouaoudia <malik@bouaoudia.com>
2023-03-24 16:14:38 +0000

    refactored routes file

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "events#my_events"
 
+  resources :events, only: [:show, :new, :create, :edit, :update, :destroy]
   get "my_events", to: "events#my_events", as: :my_events
-  get "events/new", to: "events#new"
-  post "events", to: "events#create"
 
-  get "events/:id", to: "events#show", as: :event
-
-
-  get "events/:id/edit", to: "events#edit", as: :edit_event
-  patch "events/:id", to: "events#update"
-
-  delete "events/:id", to: "events#destroy"
 end
