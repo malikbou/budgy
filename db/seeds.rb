efa31f4e3b87067cc2d11464910602a04145f5e7
Malik Bouaoudia <malik@bouaoudia.com>
2023-03-24 11:59:15 +0000

    added events for admin in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+
+Event.destroy_all
+puts "Destroyed events"
+
+User.destroy_all
+puts "Destroyed users"
+puts "\n"
 
 puts "Creating users..."
 user1 = {
@@ -19,3 +20,33 @@ user1 = {
 end
 puts "Finished users!"
 puts "\n"
+
+puts "checking something"
+user_id = User.first.id
+puts "ID retrieved: #{user_id}"
+
+puts "Creating events..."
+lisbon = {user_id: user_id,
+  name: "Lisboa",
+  description: "The bois in Lisbon",
+  category: "Trip",
+  people: 5,
+  start_date: "2023-01-01",
+  end_date: "2023-01-30",
+  budget: false,
+  currency: 'EUR'}
+madrid = {user_id: user_id,
+  name: "Madrid",
+  description: "The bois in Madrid",
+  category: "Trip",
+  people: 8,
+  start_date: "2023-02-14",
+  end_date: "2023-02-25",
+  budget: false,
+  currency: 'EUR'}
+
+[lisbon, madrid].each do |attributes|
+  event = Event.create!(attributes)
+  puts "Created #{event.name}"
+end
+puts "Finished!"
