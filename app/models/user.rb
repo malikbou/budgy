8ab3d23b155abd971da8a6e2f268fe419d611cd4
Malik Bouaoudia <malik@bouaoudia.com>
2023-03-24 11:29:47 +0000

    added has many events to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :email, format: { with: /\A.*@.*\.com\z/ }
+  has_many :events
 end
