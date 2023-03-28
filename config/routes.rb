6373ebeabe1e6529849dd859c8c1e348b6069e28
Malik Bouaoudia <malik@bouaoudia.com>
2023-03-28 13:34:29 +0100

    added nested routes for event_budgets

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "events#my_events"
 
-  resources :events, only: [:show, :new, :create, :edit, :update, :destroy]
+  resources :events, only: [:show, :new, :create, :edit, :update, :destroy] do
+    resources :event_budgets, only: [:new, :create, :edit, :update, :show]
+  end
   get "my_events", to: "events#my_events", as: :my_events
+
+# event_event_budgets       POST   /events/:event_id/event_budgets(.:format)                     event_budgets#create
+# new_event_event_budget    GET    /events/:event_id/event_budgets/new(.:format)                 event_budgets#new
+# edit_event_event_budget   GET    /events/:event_id/event_budgets/:id/edit(.:format)            event_budgets#edit
+# event_event_budget        GET    /events/:event_id/event_budgets/:id(.:format)                 event_budgets#show
+#                           PATCH  /events/:event_id/event_budgets/:id(.:format)                 event_budgets#update
+#                           PUT    /events/:event_id/event_budgets/:id(.:format)                 event_budgets#update
 
 end
