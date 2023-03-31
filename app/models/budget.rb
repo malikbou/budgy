afc4b55f390cc0d23fc329f70472fe99f1db74b7
Malik Bouaoudia <malik@bouaoudia.com>
2023-03-31 17:28:02 +0100

    managed to incorporate dropdown menu for expense categories

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,4 +1,10 @@
 class Budget < ApplicationRecord
   belongs_to :user
   belongs_to :event
+
+  def self.all_columns
+    column_names.reject { |column| %w[id user_id event_id created_at updated_at].include?(column) }.map do |column_name|
+      [Budget.human_attribute_name(column_name), column_name]
+    end
+  end
 end
